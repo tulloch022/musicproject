06a073e8690f4f397bf659fcc71940f685591e9e
Tulloch022 <90489018+tulloch022@users.noreply.github.com>
2022-05-09 14:37:10 -0700

    first commit

---
+++ src/Components/App/SearchBar/SearchBar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+class SearchBar extends React.Component {
+    render() {
+        return (
+            <div class="SearchBar">
+                <input placeholder="Enter A Song, Album, or Artist" />
+                <button className="SearchButton">SEARCH</button>
+            </div>
+        )
+    }
+}
+
+
+export default SearchBar;
